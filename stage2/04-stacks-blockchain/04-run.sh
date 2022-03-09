706cdf2a66003de649206ff217982c499d41e77c
Jasper <7660824+Ceramicwhite@users.noreply.github.com>
2022-03-08 16:02:53 -0800

    Update 04-run.sh

--- stage2/04-stacks-blockchain/04-run.sh
+++ stage2/04-stacks-blockchain/04-run.sh
@@ -5,8 +5,9 @@
   cp mainnet-conf.toml "${ROOTFS_DIR}/etc/stacks-blockchain/"
   cp bashrc "${ROOTFS_DIR}/home/stacks/"
   #mv "${ROOTFS_DIR}/home/stacks/bashrc" "${ROOTFS_DIR}/home/stacks/.bashrc"
-  mkdir -p bin/stacks-node
-  cd bin/stacks-node
+  mkdir -p bin
+  cd bin
   wget https://github.com/stacks-network/stacks-blockchain/releases/download/2.05.0.1.0/linux-arm64.zip
   unzip linux-arm64.zip
+  rm linux-arm64.zip
   install -m 0755 -o root -g root -t "${ROOTFS_DIR}/usr/local/bin/" *
